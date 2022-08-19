80b3b3068930c6c163aa8c8047a8c21ce2d0e8bf
ChappotaCompany <suman.venkat@chappota.com>
2022-08-19 06:08:46 +0000

    First Commit

---
+++ readytobill/README.md
@@ -0,0 +1,46 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Fri Aug 19 2022 05:22:02 GMT+0000 (Coordinated Universal Time)|
+|**App Generator**<br>@sap/generator-fiori-freestyle|
+|**App Generator Version**<br>1.7.1|
+|**Generation Platform**<br>SAP Business Application Studio|
+|**Floorplan Used**<br>1worklist|
+|**Service Type**<br>SAP System (ABAP On Premise)|
+|**Service URL**<br>https://my304609-api.s4hana.ondemand.com/sap/opu/odata/sap/YY1_WIPENTRIES_CDS/
+|**Module Name**<br>readytobill|
+|**Application Title**<br>Ready To Bill|
+|**Namespace**<br>com.chappota.readytobill|
+|**UI5 Theme**<br>sap_horizon|
+|**UI5 Version**<br>1.102.4|
+|**Enable Code Assist Libraries**<br>False|
+|**Add Eslint configuration**<br>False|
+|**Object collection**<br>YY1_WIPENTRIES|
+|**Object collection key**<br>SAP_UUID|
+|**Object ID**<br>BasePrice|
+|**Object number**<br>BasePrice|
+|**Object unit of measure**<br>BasePrice|
+
+## readytobill
+
+Ready To Bill
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply run the following from the generated app root folder:
+
+```
+    npm start
+```
+
+- It is also possible to run the application using mock data that reflects the OData Service URL supplied during application generation.  In order to run the application with Mock Data, run the following from the generated app root folder:
+
+```
+    npm run start-mock
+```
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ readytobill/webapp/test/flpSandbox.js
@@ -0,0 +1,100 @@
+sap.ui.define([
+	"sap/base/util/ObjectPath",
+	"sap/ushell/services/Container"
+], function (ObjectPath) {
+	"use strict";
+
+	// define ushell config
+	ObjectPath.set(["sap-ushell-config"], {
+		defaultRenderer: "fiori2",
+		bootstrapPlugins: {
+			"RuntimeAuthoringPlugin": {
+				component: "sap.ushell.plugins.rta",
+				config: {
+					validateAppVersion: false
+				}
+			},
+			"PersonalizePlugin": {
+				component: "sap.ushell.plugins.rta-personalize",
+				config: {
+					validateAppVersion: false
+				}
+			}
+		},
+		renderers: {
+			fiori2: {
+				componentData: {
+					config: {
+						enableSearch: false,
+						rootIntent: "Shell-home"
+					}
+				}
+			}
+		},
+		services: {
+			"LaunchPage": {
+				"adapter": {
+					"config": {
+						"groups": [{
+							"tiles": [{
+								"tileType": "sap.ushell.ui.tile.StaticTile",
+								"properties": {
+									"title": "Ready To Bill",
+									"targetURL": "#comchappotareadytobillreadytobill-display"
+								}
+							}]
+						}]
+					}
+				}
+			},
+			"ClientSideTargetResolution": {
+				"adapter": {
+					"config": {
+						"inbounds": {
+							"comchappotareadytobillreadytobill-display": {
+								"semanticObject": "comchappotareadytobillreadytobill",
+								"action": "display",
+								"description": "Ready To Bill",
+								"title": "Ready To Bill",
+								"signature": {
+									"parameters": {}
+								},
+								"resolutionResult": {
+									"applicationType": "SAPUI5",
+									"additionalInformation": "SAPUI5.Component=com.chappota.readytobill.readytobill",
+									"url": sap.ui.require.toUrl("com/chappota/readytobill/readytobill")
+								}
+							}
+						}
+					}
+				}
+			},
+			NavTargetResolution: {
+				config: {
+					"enableClientSideTargetResolution": true
+				}
+			}
+		}
+	});
+
+	var oFlpSandbox = {
+		init: function () {
+			/**
+			 * Initializes the FLP sandbox
+			 * @returns {Promise} a promise that is resolved when the sandbox bootstrap has finshed
+			 */
+
+			// sandbox is a singleton, so we can start it only once
+			if (!this._oBootstrapFinished) {
+				this._oBootstrapFinished = sap.ushell.bootstrap("local");
+				this._oBootstrapFinished.then(function () {
+					sap.ushell.Container.createRenderer().placeAt("content");
+				});
+			}
+
+			return this._oBootstrapFinished;
+		}
+	};
+
+	return oFlpSandbox;
+});
